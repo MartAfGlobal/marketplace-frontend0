e1f53808fdacff43abb014922a187ffca1b7f380
tceeokoye <tceeservices@gmail.com>
2025-10-02 21:21:44 +0100

    new

--- src/components/ui/buyer-components/profile-picture.tsx
+++ src/components/ui/buyer-components/profile-picture.tsx
@@ -5,7 +5,7 @@ import { ProfileImageProps } from "@/types/global";
 
 export default function ProfileImage({
   src,
-  alt = "Profile Image",
+  alt = "Profile Image",  
   onEditClick,
 }: ProfileImageProps) {
   let safeSrc: string | StaticImageData = profilePicture;
