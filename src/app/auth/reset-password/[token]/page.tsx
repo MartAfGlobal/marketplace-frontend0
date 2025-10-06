4caae2ff801fb5abf019f93026631c53a73ae276
tceeokoye <tceeservices@gmail.com>
2025-10-06 19:42:52 +0100

    new

--- src/app/auth/reset-password/[token]/page.tsx
+++ src/app/auth/reset-password/[token]/page.tsx
@@ -8,7 +8,7 @@ import ResetPasswordForm from "@/components/ui/forms/auth/buyer/resetPassword";
 export default function ResetPasswordPage() {
   return (
     <AuthenticationLayout
-      title="Reset your password"
+      title="Reset your password."
       description="Enter a new password for your account"
     >
       <div>
