477145d390d44e4f97690341c79a7c418b288282
tceeokoye <tceeservices@gmail.com>
2026-02-06 19:21:21 +0100

    new commit

--- src/components/ui/buyer-components/orders/orders.tsx
+++ src/components/ui/buyer-components/orders/orders.tsx
@@ -38,7 +38,7 @@ export default function OrdersPage() {
 
   return (
     <div className="w-full  justify-center  flex flex-col">
-      <div className="flex justify-between sticky top-16 z-10 bg-white pt-c32 border-b border-black/10 w-full overflow-x-auto">
+      <div className="flex justify-between sticky top-12 md:top-16 z-10 bg-white pt-c32 border-b border-black/10 w-full overflow-x-auto">
         <OrdersNav
           tabs={tabs}
           activeTab={activeTab}
